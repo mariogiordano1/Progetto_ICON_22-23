34a72d2e634e1e7a4b196729da0a341cf480793a
mariogiordano1 <mariogiordano0405@gmail.com>
2023-06-18 20:44:00 +0200

    created API_data.json

--- src/aqicn_data.py
+++ src/aqicn_data.py
@@ -6,19 +6,19 @@ import pandas as pd
 
 def get_data(file_path):
     count = 0
-    df = pd.read_csv(file_path)
+    df = pd.read_csv(file_path, encoding="utf-16")
     lat_list = df["lat"].tolist()
     long_list = df["long"].tolist()
-    for lat, long in zip(lat_list, long_list):
-        print(lat,long)
-        count = count + 1
-        r = requests.get(
-            f"""https://api.waqi.info/feed/geo:{lat};{long}/?token=12f1f5758ff0027428dd7a64bbe1c50c7b10206b""")
-        print(r.json())
-        data = r.json()
-        with open('Database/JSONS/API_data.json', 'w') as f:
-            json.dump(data, f)
-        print("Finished " + str(count))
+    with open('Database/JSONS/API_data.json', 'w', encoding="utf-16") as f:
+        for lat, long in zip(lat_list, long_list):
+            print(lat,long)
+            count = count + 1
+            r = requests.get(
+                f"""https://api.waqi.info/feed/geo:{lat};{long}/?token=12f1f5758ff0027428dd7a64bbe1c50c7b10206b""")
+            print(r.json())
+            data = r.json()
+            f.write(str(data) + "\n")
+            print("Finished " + str(count))
 
 
 
