91ed0535c5caec902785fbfe40bd6efda00b4001
Patrick Clark <patrickclark@outlook.it>
2023-06-18 19:07:51 +0200

    solved error

--- src/aqicn_data.py
+++ src/aqicn_data.py
@@ -9,16 +9,16 @@ def get_data(file_path):
     df = pd.read_csv(file_path)
     lat_list = df["lat"].tolist()
     long_list = df["long"].tolist()
-    for lat, long in zip(lat_list, long_list):
-        print(lat,long)
-        count = count + 1
-        r = requests.get(
-            f"""https://api.waqi.info/feed/geo:{lat};{long}/?token=12f1f5758ff0027428dd7a64bbe1c50c7b10206b""")
-        print(r.json())
-        data = r.json()
-        with open('Database/JSONS/API_data.json', 'w') as f:
-            json.dump(data, f)
-        print("Finished " + str(count))
+    with open('Database/JSONS/API_data2.json', 'w') as f:
+        for lat, long in zip(lat_list, long_list):
+            print(lat,long)
+            count = count + 1
+            r = requests.get(
+                f"""https://api.waqi.info/feed/geo:{lat};{long}/?token=12f1f5758ff0027428dd7a64bbe1c50c7b10206b""")
+            print(r.json())
+            data = r.json()
+            f.write(str(data) + "\n")
+            print("Finished " + str(count))
 
 
 
