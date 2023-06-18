40f845ab268149afc32c61399dd93356f0b66cc0
Patrick Clark <patrickclark@outlook.it>
2023-06-18 17:10:47 +0200

    push

--- src/aqicn_data.py
+++ src/aqicn_data.py
@@ -1,11 +1,33 @@
-from collections import namedtuple
+import requests
+import json
+import csv
+import pandas as pd
 
-import aqilib.aqicn
-import aqilib.utils
-from src.aqilib import aqicn, utils
 
-api = aqicn.AqicnApi(secret="12f1f5758ff0027428dd7a64bbe1c50c7b10206b")
-ip_based_data = api.get_feed()
-utils.scrap_data_from_website()
-Coordinate = namedtuple('Coordinate', ['lat', 'lng'])
-api.get_location_feed(Coordinate(lat=10.3, lng=20.7))
+def get_data(file_path):
+    count = 0
+    df = pd.read_csv(file_path)
+    lat_list = df["lat"].tolist()
+    long_list = df["long"].tolist()
+    for lat, long in zip(lat_list, long_list):
+        print(lat,long)
+        count = count + 1
+        r = requests.get(
+            f"""https://api.waqi.info/feed/geo:{lat};{long}/?token=12f1f5758ff0027428dd7a64bbe1c50c7b10206b""")
+        print(r.json())
+        if r.status_code != 200:
+            raise ValueError
+        data = r.json()
+        with open('Database/JSONS/API_data.json', 'w') as f:
+            json.dump(data, f)
+        print("Finished " + str(count))
+
+
+
+
+
+
+get_data("Database/CSVS/Universities_No_Dupes.csv")
+'''
+  
+'''
