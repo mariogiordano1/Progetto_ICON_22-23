d1b882e91d865e3638bad5a30d7b218fda3738b7
Patrick Clark <patrickclark@outlook.it>
2023-06-27 16:52:46 +0200

    push all

--- src/aqicn_data.py
+++ src/aqicn_data.py
@@ -1,10 +1,10 @@
+import csv
+
 import requests
 import json
-import csv
 import pandas as pd
 from pathlib2 import Path
-import json
-
+import re
 
 
 def get_data(file_path):
@@ -14,17 +14,16 @@ def get_data(file_path):
     long_list = df["long"].tolist()
     with open(f"""Database/JSONS/API_data_TESTING.json""", 'a', encoding="utf-16") as f:
         for lat, long in zip(lat_list, long_list):
-                print(lat, long)
-                count = count + 1
-                r = requests.get(
-                        f"""https://api.waqi.info/feed/geo:{lat};{long}/?token=12f1f5758ff0027428dd7a64bbe1c50c7b10206b""")
-                print(r.json())
-                data = r.json()
-                f.write("\n")
-                json.dump(data, f)
-                print("Finished " + str(count))
+            print(lat, long)
+            count = count + 1
+            r = requests.get(
+                f"""https://api.waqi.info/feed/geo:{lat};{long}/?token=12f1f5758ff0027428dd7a64bbe1c50c7b10206b""")
+            print(r.json())
+            data = r.json()
+            f.write("\n")
+            json.dump(data, f)
+            print("Finished " + str(count))
         f.write("_")
-
 
 
 def change_encoding(file_path):
@@ -32,6 +31,7 @@ def change_encoding(file_path):
         with open("Database/JSONS/API_data_TESTING_decoded.json", 'w+b') as dest_file:
             contents = source_file.read()
             dest_file.write(contents.decode('utf-16').encode('windows-1252', errors='replace'))
+
 
 def format_file(file_path):
     file = Path(file_path)
@@ -44,13 +44,14 @@ def format_file(file_path):
     remove_blank_spaces_txt()
 
 
-
 def remove_blank_spaces_txt():
-    with open("Database/JSONS/API_Data_TESTING.json", 'r') as r, open("Database/JSONS/API_Data_Complete.json", 'w') as o:
+    with open("Database/JSONS/API_Data_TESTING.json", 'r') as r, open("Database/JSONS/API_Data_Complete.json",
+                                                                      'w') as o:
         for line in r:
             # strip() function
             if line.strip():
                 o.write(line)
+
 
 def append_parent():
     file = open("Database/JSONS/API_Data_Complete.txt", mode='a+', encoding='windows-1252')
@@ -60,9 +61,21 @@ def append_parent():
     p.rename(p.with_suffix('.json'))
 
 
-#get_data("Database/CSVS/Universities_No_Dupes.csv")
-#change_encoding("Database/JSONS/API_data_TESTING.json")
-format_file("Database/JSONS/API_data_TESTING.json")
+def filter_data(file_path):
+    with open(file_path) as f:
+        file_to_list = f.read().splitlines()
+        with open("Database/JSONS/test.txt", 'w') as o:
+            for line in file_to_list:
+                text = re.findall('"attribute":', line)
+                o.write(str(text))
+
+
+
+
+# get_data("Database/CSVS/Universities_No_Dupes.csv")
+# change_encoding("Database/JSONS/API_data_TESTING.json")
+# format_file("Database/JSONS/API_data_TESTING.json")
+#filter_data("Database/JSONS/API_Data_Complete.json")
 
 
 '''
